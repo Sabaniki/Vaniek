0f128300fddc90afd2cf764e7dff86223dc1f703
Sabaniki <sawada13@mccc.jp>
2020-01-12 04:51:09 +0900

    なぜかステージングしてなくて萎えてる

---
+++ src/main/kotlin/saba/main/Main.kt
@@ -0,0 +1,12 @@
+package saba.main
+
+import saba.repl.Repl
+
+class Main{
+    fun main(args: Array<String>) {
+        val repl = Repl()
+        println("Hello")
+        repl.start()
+    }
+}
+
---
+++ src/test/kotlin/saba/MainTest.kt
@@ -0,0 +1,8 @@
+package saba
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class MainTest {
+
+}
